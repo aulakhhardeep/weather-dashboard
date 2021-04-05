42f950f3b5a52b5f1cc199f75d731c9b12818d28
Hardeep <aulakh.hardeep91@gmail.com>
2021-04-04 20:03:58 -0400

    added initPage function to get value using javaScript

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+}
+initPage();
